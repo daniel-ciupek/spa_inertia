ce77655c2f0db0c5065d7e21c9fe33538d1b3843
daniel-ciupek <danter005@o2.pl>
2026-03-11 20:27:34 +0100

    Working with Model Facotires

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,26 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
+   
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $categories = Category::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory(5)
+            ->has(
+                Product::factory(10)->state(function () use ($categories) {
+                    return ['category_id' => $categories->random()->id];
+                })
+            )
+            ->create();
     }
 }
