b29f6632bf1d4d37f5e7db6067fef4f54b5f564a
daniel-ciupek <danter005@o2.pl>
2026-03-15 13:17:48 +0100

    Refactoring our components

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use App\Models\Category;
-use Illuminate\Http\Request;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\CategoryResource;
 use App\Http\Requests\StoreProductRequest;
